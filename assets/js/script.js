0e146a14bdc1afd4d04d8328ae82dc86b675d80b
Cécile Lafont <104426870+cecdeweb@users.noreply.github.com>
2023-06-01 16:28:22 +0200

    fix validator w3c

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const successStory = document.getElementById('success-story');
+
+successStory.addEventListener('mouseover', () => {
+        successStory.style.bottom = '-400px';
+    });
+
+successStory.addEventListener('mouseout', () => {
+        successStory.style.bottom = '-772px';
+    });
