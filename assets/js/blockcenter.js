8918b6f8f8b955a44f1f067058fbb0f71451545c
Cécile Lafont <104426870+cecdeweb@users.noreply.github.com>
2023-06-03 22:44:29 +0200

    feature 404 page tv template with js centering

--- assets/js/blockcenter.js
+++ assets/js/blockcenter.js
@@ -1,10 +1,33 @@
 window.addEventListener('resize', function() {
-  var circle = document.querySelector('.circle');
-  var width = circle.offsetWidth;
-  var leftValue = window.innerWidth/2 - (width/2);
-  var topValue = window.innerWidth/2 - (width/2);
-  circle.style.height = width + 'px';
-  circle.style.left = leftValue + 'px';
-  circle.style.top = topValue + 'px';
+    var circle = document.querySelector('.circle');
+    if (window.innerWidth > window.innerHeight) {
+        circle.style.height = 80 + '%';
+        var height = circle.offsetHeight;
+        circle.style.width = height + 'px';
+        var leftValue = (window.innerWidth - height) / 2;
+        var topValue = (window.innerHeight - height) / 2;
+        circle.style.left = leftValue + 'px';
+        circle.style.top = topValue + 'px';
+    } else {
+        circle.style.width = 80 + '%';
+        var width = circle.offsetWidth;
+        circle.style.height = width + 'px';
+        var leftValue = (window.innerWidth - width) / 2;
+        var topValue = (window.innerHeight - width) / 2;
+        circle.style.left = leftValue + 'px';
+        circle.style.top = topValue + 'px';
+  }
 });
+
 window.dispatchEvent(new Event('resize'));
+
+// window.addEventListener('resize', function() {
+//   var circle = document.querySelector('.circle');
+//   var width = circle.offsetWidth;
+//   var leftValue = window.innerWidth/2 - (width/2);
+//   var topValue = window.innerWidth/2 - (width/2);
+//   circle.style.height = width + 'px';
+//   circle.style.left = leftValue + 'px';
+//   circle.style.top = topValue + 'px';
+// });
+// window.dispatchEvent(new Event('resize'));
